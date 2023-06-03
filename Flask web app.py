e9d84c8b39e415de7dcf694437eb9f2d71ccd2ee
devbox-bhanu <134588735+devbox-bhanu@users.noreply.github.com>
2023-06-03 18:08:53 +0530

    Add files via upload

---
+++ Flask web app.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from flask import flask
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    date = datetime.now()
+    return str(date)
+
+if _name_ == "_main_":
+    app.run(host='0.0.0.0',host=5000)
